d015533431d8913cad5782a54a1af2df2ed85f30
Asier Cardoso <asier314@gmail.com>
2018-01-27 02:38:28 +0100

    Add version file

---
+++ anzan.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from random import random, randint
+
+
+def anzan_addition(times=5, digits=1, fixed=True, negative=False, negative_proability=0.5):
+    numbers = []
+    result = 0
+
+    range_max = 10**digits-1
+    range_min = 10**(digits-1)
+
+    for i in range(times):
+        if fixed:
+            n = randint(range_min, range_max)
+        else:
+            n = randint(1, range_max)
+
+        if negative and random() < negative_proability:
+            n = -n
+
+        print(n)
+
+        numbers.append(n)
+        result += n
+
+    return numbers, result
+
+
