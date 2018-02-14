4cb7c17c572fad96fc01d256cc937140f7bfaa82
Asier Cardoso <asier314@gmail.com>
2018-02-15 00:07:09 +0100

    Add settings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.screenmanager import NoTransition
+from kivy.uix import settings
+from kivy.config import ConfigParser
 from version import __version__
 
 class MainMenuScreen(Screen):
@@ -24,9 +26,21 @@ class ScreenMan(ScreenManager):
 
 
 class AnzandoApp(App):
+    use_kivy_settings = False
+
     def build(self):
+        self.settings_cls = settings.SettingsWithSidebar
         return ScreenMan(transition=NoTransition())
 
+    def build_config(self, config):
+        config.setdefaults('main', {
+            'name': 'player1',
+            'language': 'English'
+        })
+
+    def build_settings(self, settings):
+        settings.add_json_panel('Settings',
+            self.config, filename='settings.json')
 
 if __name__ == '__main__':
     AnzandoApp().run()
